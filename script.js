373cc9876f824cbcb0f233dccc67449f16a8a42c
citylock <citylock77@gmail.com>
2017-04-14 07:40:27 +1000

    sec5

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
-// Lecture 37. 5-mins HTML and CSS
 
+// Primitives VS objects
+
+var a = 24;
+var b = a;
+a = 46;
+
+console.log(a);
+console.log(b);
+
+var obj1 = {
+    name: 'John', 
+    age: 26;
+}
+
+var obj2 = obj1;
+obj.age = 30;
+
+console.log(obj1,  age1, )
+
+)
